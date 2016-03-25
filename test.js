561355730b8ecf1391e92f08fb839d53a676c312
Chad Hollman <hollmanchad@gmail.com>
2016-03-25 14:35:22 -0700

    Implementing testing

---
+++ test.js
@@ -0,0 +1,20 @@
+/* jshint esversion: 6 */
+/* jshint node: true */
+/* jshint mocha: true */
+"use strict";
+var request = require('supertest');
+var app = require('./server/app');
+
+describe('Requests to the root path', function() {
+    it('Returns a 200 status code', function(done) {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+
+    it('Returns a HTML format', function(done) {
+        request(app)
+            .get('/')
+            .expect('Content-Type', 'text/html; charset=utf-8', done);
+    });
+});
