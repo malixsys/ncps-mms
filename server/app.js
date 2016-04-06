3151b0c6aebf53019a48c989add52834caa60f82
Chad Hollman <hollmanchad@gmail.com>
2016-04-06 13:02:01 -0700

    Moved this out of the routes for cleaner code

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 let app = express();
 let jwt = require('jsonwebtoken');
+let expressJwt = require('express-jwt');
 var config = require('./config');
 var passport = require('passport');
 
@@ -17,21 +18,19 @@ mongoose.connect('mongodb://localhost/ncps', (err) => {
         console.log('Error connecting to Mongo - make sure mongo is running...');
         process.exit(1);
     }
-
     console.log('Connected to Mongo');
 });
 
 require('./models/member');
 require('./models/user');
-
 require('./config/passport');
 
 var members = require('./routes/member');
 
+app.use('/members', expressJwt({ secret: config.secret }));
 app.use('/members', members);
 
-//var port = process.env.port || 3030;
-var port = 3030;
+var port = process.env.port || 3030;
 
 if (!module.parent) {
     app.listen(port, () => {
